db03b065d04aaf748ba0819d3992211c11334a9b
anandk <kanandaiah9@gmail.com>
2023-04-20 19:57:35 +0530

    mongod modify conf

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,10 @@
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m>>>>>>>>copy mongo repo <<<<<<<<<<\e[0m"
+cp /root/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m>>>>>>>>install mongo file <<<<<<<<<<\e[0m"
   yum install mongodb-org -y
+  echo -e "\e[36m>>>>>>>>update ports <<<<<<<<<<\e[0m"
   sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+  echo -e "\e[36m>>>>>>>>restart service <<<<<<<<<<\e[0m"
   systemctl enable mongod
   systemctl restart mongod
 
