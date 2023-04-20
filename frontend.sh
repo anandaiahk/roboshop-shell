dc748303a153dc43dbc6774c2905f9655a6e56d2
anandk <kanandaiah9@gmail.com>
2023-04-20 19:54:14 +0530

    mongod modify conf

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,14 @@
+echo -e "\e[36m>>>>>>>>install nginx <<<<<<<<<<\e[0m"
 yum install nginx -y
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -conf filee "\e[36m>>>>>>>>copy  <<<<<<<<<<\e[0m"
+cp /root/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[36m>>>>>>>>remove nginx files <<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+echo -e "\e[36m>>>>>>>>download artifacts <<<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
+echo -e "\e[36m>>>>>>>extract zip file<<<<<<<<<<\e[0m"
 unzip /tmp/frontend.zip
+echo -e "\e[36m>>>>>>>>restart service <<<<<<<<<<\e[0m"
 systemctl restart nginx
 systemctl enable nginx
