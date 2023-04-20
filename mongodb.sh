c0178344048147a0dd77f46224c20dbb889d8c3c
anandk <kanandaiah9@gmail.com>
2023-04-20 22:29:16 +0530

    mongod modify conf

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,7 @@
-echo -e "\e[36m>>>>>>>>copy mongo repo <<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-echo -e "\e[36m>>>>>>>>install mongo file <<<<<<<<<<\e[0m"
-  yum install mongodb-org -y
-  echo -e "\e[36m>>>>>>>>update ports <<<<<<<<<<\e[0m"
-  sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
-  echo -e "\e[36m>>>>>>>>restart service <<<<<<<<<<\e[0m"
-  systemctl enable mongod
-  systemctl restart mongod
+yum install mongodb-org -y
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+systemctl enable mongod
+systemctl start mongod
+systemctl restart mongod
 
