bbb2da71ebced8af4a830bd41728049008268651
anandk <kanandaiah9@gmail.com>
2023-04-21 19:32:22 +0530

    create user

---
+++ user.sh
@@ -0,0 +1,15 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+unzip /tmp/user.zip
+npm install
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
+mongo --host mongodb-dev.kanand.online </app/schema/user.js
